b0dc9e1895514bcded10666a6d724f3499838610
Hoang Xuan Thanh <thanhhoangnvbg@gmail.com>
2026-09-21 13:52:01 +0700

    refactor: add standard academic API aliases getSuccessors, testGoal, and search

--- src/sokoban/problem.py
+++ src/sokoban/problem.py
@@ -8,6 +8,10 @@ class SokobanProblem:
         self.initial_state=State(board.initial_player, board.initial_boxes)
         self.heuristic_model=ReversePushHeuristic(board)
     def get_successors(self, state): return state.successors(self.board)
+    def getSuccessors(self, state): return self.get_successors(state)
     def is_goal(self, state): return state.boxes == self.board.goals
+    def test_goal(self, state): return self.is_goal(state)
+    def testGoal(self, state): return self.is_goal(state)
     def step_cost(self, *_): return 1
     def heuristic(self, state): return self.heuristic_model(state)
+
--- src/sokoban/search/astar.py
+++ src/sokoban/search/astar.py
@@ -1,2 +1,4 @@
 from .common import graph_search
 def solve(problem): return graph_search(problem, use_heuristic=True)
+search = solve
+
